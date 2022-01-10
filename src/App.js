da31f35c3d03b6075284025ef1f4e3745a4b4783
TatyanaLikar <85054268+TanyanaLikar@users.noreply.github.com>
2022-01-10 20:42:59 +0200

    create base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { GlobalStyle } from './components/GlobalStyle';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
